7f11140314d9bc71d0b67e138e7891f68ae7faf0
takiido <takiid0@proton.me>
2025-08-26 20:29:30 -0500

    removed themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,11 @@
 
 import Button from "./components/ui/Button";
 import "../styles/globals.scss";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
-  
-  const handleThemeChange = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  }
-
-  useEffect(() => {
-    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setTheme(isDark ? 'dark' : 'light');
-  }, []);
-
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-  }, [theme]);
-
   return (
     <div className="container">
-      <Button text="Click me" onClick={handleThemeChange} disabled={false} />
+      <Button text="Click me" onClick={() => console.log("Button clicked")} disabled={false} />
       <Button type="secondary" text="Click me" onClick={() => console.log("Button clicked")} disabled={false} />
       <Button type="accent" text="Click me" onClick={() => console.log("Button clicked")} disabled={false} />
     </div>
